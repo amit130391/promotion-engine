162f4db7f2bf50a90f8ec72943da6bc8afdc287f
Amit Sharma <amit130391@gmail.com>
2025-07-06 06:13:52 +0530

    Initial commit of Promotion Engine project

---
+++ src/test/java/com/godash/promotion_engine/PromotionEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.godash.promotion_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromotionEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
